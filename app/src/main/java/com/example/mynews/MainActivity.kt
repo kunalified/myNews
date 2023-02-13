461d247321da39175007416a5fbe3cd9c1e904c4
Kunal Singh <kunal17leon@gmail.com>
2023-02-13 19:58:41 +0530

    First Commit

---
+++ app/src/main/java/com/example/mynews/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.mynews
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import vie
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        binding.recyclerView.layout
+
+    }
+}
---
+++ app/src/main/java/com/example/mynews/NewsModal.java
@@ -0,0 +1,4 @@
+package com.example.mynews;
+
+public class NewsModal {
+}
